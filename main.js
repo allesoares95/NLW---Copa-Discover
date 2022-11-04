becb07a799a72655c5cd598dc9d7f22724f63a88
allesoares95 <allesoares_@live.com>
2022-11-03 21:29:07 -0300

    updateMain

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ function createGame(player1, hour, player2) {
   `
 }
 
+
 function createCard(date, day, games) {
   return `
   <div class="card">
@@ -20,27 +21,25 @@ function createCard(date, day, games) {
 }
 
 
-document.querySelector("#app").innerHTML = `
-  <header>
-    <img src="./assets/logo.svg" alt="Logo da NLW">
-   </header>
-            
-   <main id="cards">
+//  Criação de HTML interno direto no id.app Template Literals(document.querySelector("#app").innerHTML = `)
+
+document.querySelector("#cards").innerHTML = ` 
+         
     ${createCard(
       "24/11",
       "quinta",
       createGame("suica", "07:00", "camaroon") +
-        createGame("uruguai", "10:00", "korea") +
-        createGame("portugal", "10:00", "ghana") +
-        createGame("brazil", "16:00", "servia")
+      createGame("uruguai", "10:00", "korea") +
+      createGame("portugal", "10:00", "ghana") +
+      createGame("brazil", "16:00", "servia")
     )} 
     ${createCard(
       "28/11",
       "segunda",
       createGame("camaroon", "07:00", "servia") +
-        createGame("korea", "10:00", "ghana") +
-        createGame("brazil", "13:00", "suica") +
-        createGame("portugal", "16:00", "uruguai")
+      createGame("korea", "10:00", "ghana") +
+      createGame("brazil", "13:00", "suica") +
+      createGame("portugal", "16:00", "uruguai")
     )}
     ${createCard(
       "02/12",
@@ -50,5 +49,4 @@ document.querySelector("#app").innerHTML = `
       createGame("servia", "16:00", "suica") +
       createGame("camaroon", "16:00", "brazil")
     )}
-  </main>
 `
